6b9db04a4e4a57e319fcdab2f9891e80f818bb5d
MGlolenstine <mglolenstine@gmail.com>
2024-09-09 15:23:23 +0200

    Fixed parsing of the header.

--- src/models/gif.rs
+++ src/models/gif.rs
@@ -1,14 +1,51 @@
 use bypar::prelude::UnsizedVec;
 use bypar_derive::{FromBytes, ToBytes};
 
-#[derive(Clone, Debug, FromBytes, ToBytes)]
+#[derive(Clone, Debug, ToBytes)]
 pub struct Gif {
-    // magic_part1: u32,
-    // magic_part2: u16,
     magic: UnsizedVec<u8, 6>,
 
     descriptor: LogicalScreenDescriptor,
     global_colour_table: GlobalColourTable,
+}
+
+impl bypar::FromBytes for Gif {
+    fn from_bytes(bytes: &[u8]) -> Result<Self, bypar::prelude::Error>
+    where
+        Self: Sized,
+    {
+        Ok(Self::from_byte_stream(bytes)?.0)
+    }
+
+    fn from_byte_stream(mut bytes: &[u8]) -> Result<(Self, &[u8]), bypar::prelude::Error>
+    where
+        Self: Sized,
+    {
+        let mut magic = UnsizedVec::default();
+
+        if bytes.len() < 6 {
+            return Err(bypar::prelude::Error::FailedToParse);
+        }
+
+        for _ in 0..6 {
+            let (element, remaining_bytes) = u8::from_byte_stream(bytes)?;
+            bytes = remaining_bytes;
+            magic.push(element);
+        }
+
+        let (descriptor, remaining_bytes) = LogicalScreenDescriptor::from_byte_stream(bytes)?;
+        let (global_colour_table, remaining_bytes) =
+            GlobalColourTable::from_byte_stream(remaining_bytes)?;
+
+        Ok((
+            Self {
+                magic,
+                descriptor,
+                global_colour_table,
+            },
+            remaining_bytes,
+        ))
+    }
 }
 
 #[derive(Clone, Debug, FromBytes, ToBytes)]
@@ -22,6 +59,7 @@ pub struct LogicalScreenDescriptor {
 
 #[derive(Clone, Debug, ToBytes)]
 pub struct GlobalColourTable {
+    magic: u8, // This always has to be 0
     entries: UnsizedVec<GlobalColourTableEntry, 255>,
 }
 
@@ -37,20 +75,32 @@ impl bypar::FromBytes for GlobalColourTable {
     where
         Self: Sized,
     {
-        let mut ret = UnsizedVec::default();
-        for _ in 0..255u8 {
-            let (entry, remaining_bytes) = GlobalColourTableEntry::from_byte_stream(bytes)?;
+        let (magic, remaining_bytes) = u8::from_byte_stream(bytes)?;
+        bytes = remaining_bytes;
 
-            ret.push(entry);
+        let mut ret = UnsizedVec::default();
+        for colour_index in 0..255u8 {
+            let (entry, remaining_bytes) = ColourTableEntry::from_byte_stream(bytes)?;
+
+            ret.push(GlobalColourTableEntry {
+                index: colour_index,
+                colour: entry,
+            });
 
             bytes = remaining_bytes;
         }
 
-        Ok((Self { entries: ret }, bytes))
+        Ok((
+            Self {
+                magic,
+                entries: ret,
+            },
+            bytes,
+        ))
     }
 }
 
-#[derive(Clone, Default, Debug, FromBytes, ToBytes)]
+#[derive(Clone, Default, Debug, ToBytes)]
 pub struct GlobalColourTableEntry {
     index: u8,
     colour: ColourTableEntry,
